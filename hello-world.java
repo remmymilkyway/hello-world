cecb38dad553bc0bf0c79442a9e7ff5b2999c44a
remmymilkyway <94217071+remmymilkyway@users.noreply.github.com>
2022-05-10 20:58:34 +0800

    Update hello-world.java

--- hello-world.java
+++ hello-world.java
@@ -1,6 +1,6 @@
 package Main;
 class hello-world {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("hello world"); 
     }
 }
